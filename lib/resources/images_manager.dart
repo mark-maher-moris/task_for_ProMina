de0b80f6d28e28d5d5ae2132b07b87052cf8ad32
Mark <markmaher933@gmail.com>
2024-06-03 02:02:47 +0300

    Adding Assets folder and refactor pubspec

---
+++ lib/resources/images_manager.dart
@@ -0,0 +1,7 @@
+class ImagesImages{
+static const String topArrow = "assets/images/arrow1.png";
+static const String leftArrow = "assets/images/arrow2.png";
+static const String background = "assets/images/background1.png";
+static const String camera = "assets/images/camera.png";
+static const String photo = "assets/images/photo.png";
+}
